70468d0178f0bb041289d28c6ec6ffb9cfbd2582
Ominixjs <paixaoca2003@gmail.com>
2026-03-18 21:31:31 -0300

    Adicionado sistema de login

--- app.js
+++ app.js
@@ -2,51 +2,71 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
+// Informações
+import dotenv from "dotenv";
+dotenv.config();
+
+import session from "express-session";
+
 // pasta para arquivos
 app.use(express.static("public"));
 
+// Decodifica URL
 app.use(express.urlencoded({ extended: true }));
+
+// Renderizador para o node
+app.set("view engine", "ejs");
+
+// Memoria temporária
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    rolling: true,
+    cookie: {
+      maxAge: 5000 * 60 * 60,
+    },
+  }),
+);
 
 const db = [
   {
     id: 1,
-    nome: "Ana Beatriz Lima",
-    dataNascimento: "1995-04-12",
+    name: "Ana Beatriz Lima",
+    birthday: "1995-04-12",
     email: "ana.lima@email.com",
-    senha: "ana12345",
+    password: "ana12345",
   },
   {
     id: 2,
-    nome: "Carlos Eduardo Martins",
-    dataNascimento: "1989-09-21",
+    name: "Carlos Eduardo Martins",
+    birthday: "1989-09-21",
     email: "carlos.martins@email.com",
-    senha: "carlose89",
+    password: "carlose89",
   },
   {
     id: 3,
-    nome: "Juliana Ferreira",
-    dataNascimento: "2001-01-30",
+    name: "Juliana Ferreira",
+    birthday: "2001-01-30",
     email: "juliana.ferreira@email.com",
-    senha: "juli2001",
+    password: "juli2001",
   },
   {
     id: 4,
-    nome: "Rafael Costa",
-    dataNascimento: "1993-06-18",
+    name: "Rafael Costa",
+    birthday: "1993-06-18",
     email: "rafael.costa@email.com",
-    senha: "rafa123",
+    password: "rafa123",
   },
   {
     id: 5,
-    nome: "Mariana Oliveira",
-    dataNascimento: "1998-11-05",
+    name: "Mariana Oliveira",
+    birthday: "1998-11-05",
     email: "mariana.oliveira@email.com",
-    senha: "mariolive98",
+    password: "mariolive98",
   },
 ];
-
-// Renderizador para o node
-app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -56,10 +76,31 @@ app.get("/login", (req, res) => {
   res.render("pages/login");
 });
 
+// autenticar dados
 app.post("/auth", (req, res) => {
   const { email, password } = req.body;
 
-  res.json({ email, password });
+  const client = db.find((c) => c.email == email);
+
+  if (!client) res.send("Usuário não encontrado");
+
+  if (client.password != password) res.send("Senha inválida!");
+
+  const { name, birthday } = client;
+
+  req.session.loggedUser = {
+    name,
+    email,
+    birthday,
+  };
+
+  res.redirect("/");
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/login");
+  });
 });
 
 app.listen(PORT, () => {
